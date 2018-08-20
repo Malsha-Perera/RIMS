d3ada0fa7e3f688e5289c8e010e685b7b13068f0
pHerah <herath93hmpi@gmail.com>
2018-08-20 20:02:23 +0530

    item detail changes

--- Front-end/src/app/components/inventory/item-detail/item-detail.component.ts
+++ Front-end/src/app/components/inventory/item-detail/item-detail.component.ts
@@ -4,7 +4,11 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { SetRolComponent } from './set-rol/set-rol.component';
 import { AlertComponent } from 'ngx-bootstrap/alert/alert.component';
+
+//import { Chart } from 'chart.js';
+
 import {  } from '../../../pipes/item-detail.pipe';
+//import { Chart } from 'chart.js';
 import swal from 'sweetalert2';
 
 
